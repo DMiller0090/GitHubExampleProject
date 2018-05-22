9e9691e97a19df96056a78e6f5d7009c4987df2d
dmiller0090 <pinhigh1@gmail.com>
2018-05-22 15:38:49 -0400

    Add project files.

---
+++ GitHubExampleProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitHubExampleProject.MvcApplication" Language="C#" %>
---
+++ GitHubExampleProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubExampleProject.Startup))]
+namespace GitHubExampleProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
